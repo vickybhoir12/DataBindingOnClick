44775765c3a731c7587f040ef911197911b69438
vickybhoir12 <vickybhoir12319@gmail.com>
2020-10-22 00:45:29 +0530

    test1

--- app/src/main/java/com/kotlinkatta/databindingonclick/MainActivity.kt
+++ app/src/main/java/com/kotlinkatta/databindingonclick/MainActivity.kt
@@ -2,10 +2,13 @@ package com.kotlinkatta.databindingonclick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(applicationContext,"this is test program",Toast.LENGTH_LONG).show()
     }
 }
